6376869fc758ee608e58335fbf102dd9bf5fecd0
Lance Pery <lpery@academic.rrc.ca>
2025-09-04 20:54:58 -0500

    Added the function removeExpense

--- Expense.py
+++ Expense.py
@@ -7,4 +7,16 @@ class Expense:
 class ExpenseTracker:
     def __init__(self):
         self.expenses = []
+
+    def addExpense(self, expense):
+        self.expenses.append(expense)
+
+    def removeExpense(self, index):
+        if 0 <= index < len(self.expenses):
+            del self.expenses[index]
+            print("Expense removed.")
+        else:
+            print("Invalid index. No expense removed.")
+
+        self.expenses.remove(expense)
     
