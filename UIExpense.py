b6c8b0ac20593881a74c8a5a5180d48467dccb28
Lance Pery <lpery@academic.rrc.ca>
2025-10-03 19:20:28 -0500

    Button for adding expenses now adds expenses with date, description, amount, category (optional).

--- UIExpense.py
+++ UIExpense.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 from Expense import Expense, ExpenseTracker, ExpenseDB
+import datetime
 
 
 class ExpenseApp:
@@ -35,8 +36,57 @@ class ExpenseApp:
                   command=self.exit_app).pack(pady=5)
 
     def add_expense_window(self):
-        messagebox.showinfo(
-            "Add Expene", "Here we will build Add Expense Form (Step 2)")
+        """Window to add an Expense"""
+        window = tk.Toplevel(self.root)
+        window.title("Add Expense")
+
+        # inputs
+        tk.Label(window, text="Date (YYYY-MM-DD): ").grid(row=0,
+                                                          column=0, padx=5, pady=5)
+        date_entry = tk.Entry(window)
+        date_entry.grid(row=0, column=0, padx=5, pady=5)
+        date_entry.insert(0, datetime.date.today().strftime("%Y-%m-%d"))
+
+        tk.Label(window, text="Desciption: ").grid(
+            row=0, column=0, padx=5, pady=5)
+        desc_entry = tk.Entry(window)
+        desc_entry.grid(row=1, column=1, padx=5, pady=5)
+
+        tk.Label(window, text="Amount: ").grid(row=0, column=0, padx=5, pady=5)
+        amount_entry = tk.Entry(window)
+        amount_entry.grid(row=2, column=1, padx=5, pady=5)
+
+        tk.Label(window, text="Category (optional): ").grid(
+            row=0, column=0, padx=5, pady=5)
+        category_entry = tk.Entry(window)
+        category_entry.grid(row=3, column=1, padx=5, pady=5)
+
+        def save_expense():
+            try:
+                date_val = date_entry.get()
+                datetime.datetime.strftime(
+                    date_val, "%Y-%m-%d")  # valid date format
+                description = desc_entry.get().strip()
+                amount = float(amount_entry.get().strip())
+                category = category_entry.get().strip() or None
+
+                expense = Expense(date_val, description, amount)
+                expense.category = category
+
+                self.tracker.addExpense(expense)
+                self.db.add_expense(expense)
+
+                if category and category not in self.tracker.categories:
+                    self.tracker.add_category(category)
+
+                messagebox.showinfo("Success", "Expense added succcessfully.")
+                window.destroy()
+            except ValueError:
+                messagebox.showerror(
+                    "Error", "Invalid input. Please check your entries.")
+
+            tk.Button(window, text="Add Expense", command=save_expense).grid(
+                row=4, column=0, columnspan=2, pady=10)
 
     def view_expense_window(self):
         messagebox.showinfo(
